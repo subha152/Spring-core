80b2964c0eb492b4a24e202b219dfa1b9475f74d
subha152 <subho.3600.ghosh@gmail.com>
2023-12-02 12:11:07 +0530

    spring core contructor arg

---
+++ SpringCore/src/main/java/newproject/SpringCore/contractorInjection/Certi.java
@@ -0,0 +1,22 @@
+package newproject.SpringCore.contractorInjection;
+
+public class Certi {
+	private String name;
+
+	public Certi() {
+		super();
+	}
+
+	public Certi(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Certi [name=" + name + "]";
+	}
+	
+	
+
+}
